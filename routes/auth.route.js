42aded1eaf7b533cea57d2ac2473bd9d189e3fc4
NamanMeravi <namanmeravi0@gmail.com>
2025-10-30 12:03:14 +0530

    backend deploy

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,7 +4,7 @@ import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", registerUser); 
 
 router.post("/login", Login);
 
