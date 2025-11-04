ac534f2fcb527ed0b45770250d65ee061938c578
NamanMeravi <namanmeravi0@gmail.com>
2025-11-04 10:57:46 +0530

    update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend-domain.com"],
+    origin: ["http://localhost:5173", "https://acces-lens-deploy-frontend.vercel.app"],
     credentials: true,
   })
 );
